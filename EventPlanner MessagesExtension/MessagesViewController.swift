fbd7bb4a2f1fedd2274a957f464c1f354ca06dfb
Alexis Santiago <alexissantiago1213@gmail.com>
2025-03-03 14:01:13 -0500

    -Using Deep Linking

--- EventPlanner MessagesExtension/MessagesViewController.swift
+++ EventPlanner MessagesExtension/MessagesViewController.swift
@@ -14,65 +14,23 @@ struct AddToCalendarButton: View {
     let eventName: String
     let eventLocation: String
     let eventStartDate: Date
-    let viewController: UIViewController  // Pass in a reference to the iMessage ViewController
 
     var body: some View {
         Button("Add to Calendar") {
-            saveAndOpenICSFile()
+            openCalendar()
         }
         .padding()
     }
 
-    private func saveAndOpenICSFile() {
-        let icsContent = generateICSContent()
-        if let fileURL = saveICSFile(content: icsContent) {
-            presentShareSheet(for: fileURL)
+    private func openCalendar() {
+        let timestamp = eventStartDate.timeIntervalSince1970
+        if let url = URL(string: "calshow:\(timestamp)") {
+            UIApplication.shared.open(url)
         }
     }
-
-    private func generateICSContent() -> String {
-        let dateFormatter = ISO8601DateFormatter()
-        dateFormatter.formatOptions = [.withInternetDateTime]
-
-        let startDate = dateFormatter.string(from: eventStartDate)
-        let endDate = dateFormatter.string(from: eventStartDate.addingTimeInterval(3600)) // 1-hour duration
-
-        return """
-        BEGIN:VCALENDAR
-        VERSION:2.0
-        BEGIN:VEVENT
-        SUMMARY:\(eventName)
-        LOCATION:\(eventLocation)
-        DTSTART:\(startDate)
-        DTEND:\(endDate)
-        END:VEVENT
-        END:VCALENDAR
-        """
-    }
-
-    private func saveICSFile(content: String) -> URL? {
-        let fileName = "event.ics"
-        let tempDirectory = FileManager.default.temporaryDirectory
-        let fileURL = tempDirectory.appendingPathComponent(fileName)
-
-        do {
-            try content.write(to: fileURL, atomically: true, encoding: .utf8)
-            return fileURL
-        } catch {
-            print("Error saving ICS file: \(error)")
-            return nil
-        }
-    }
-
-    private func presentShareSheet(for fileURL: URL) {
-           let activityViewController = UIActivityViewController(activityItems: [fileURL], applicationActivities: nil)
-           viewController.present(activityViewController, animated: true)
-       }
 }
 
 struct MessagesView: View {
-    let viewController: UIViewController
-
     var body: some View {
         VStack {
             Text("Event Planner")
@@ -80,8 +38,7 @@ struct MessagesView: View {
             AddToCalendarButton(
                 eventName: "YOGO Meeting",
                 eventLocation: "Discord",
-                eventStartDate: Date(),
-                viewController: viewController  // Pass reference
+                eventStartDate: Date()
             )
         }
         .padding()
@@ -89,11 +46,10 @@ struct MessagesView: View {
 }
 
 // Bridge to UIKit for iMessage extension
-class MessagesViewController: MSMessagesAppViewController, UIDocumentPickerDelegate {
+class MessagesViewController: MSMessagesAppViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let swiftUIView = UIHostingController(rootView: MessagesView(viewController: self))
+        let swiftUIView = UIHostingController(rootView: MessagesView())
         addChild(swiftUIView)
         swiftUIView.view.frame = view.frame
         view.addSubview(swiftUIView.view)
